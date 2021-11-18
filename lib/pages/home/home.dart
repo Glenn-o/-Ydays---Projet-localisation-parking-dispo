a5f695853344baae5792c6ddc62191d7cc45ccd0
Glenn Oberlé <oberle.glenn@icloud.com>
2021-11-18 10:02:33 +0100

    Ajoute google maps sur la page d'accueil

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:projet_parking/components/app_bar.dart';
 import 'package:projet_parking/components/menu.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        extendBodyBehindAppBar: true,
-        backgroundColor: Colors.transparent,
-        appBar: appBar(),
-        drawer: Menu(context),
-        body: SingleChildScrollView(
-          child: Container(
-            height: 1000,
-            color: Colors.white,
-          ),
-        ));
+      extendBodyBehindAppBar: true,
+      //backgroundColor: Colors.transparent,
+      appBar: appBar(),
+      drawer: Menu(context),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(44.837789, -0.57918),
+          zoom: 14,
+        ),
+      ),
+    );
   }
 }
